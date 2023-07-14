b04b277b751b2b8df4f621128336f311a6664978
gohome <39901387+dmobtxx19@users.noreply.github.com>
2023-07-14 21:14:01 +0900

    login success

--- lib/groupPage.dart
+++ lib/groupPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:checktrack/system/utilsSystem.dart';
 
-
 class GroupPage extends StatefulWidget {
   @override
   State<GroupPage> createState() => _GroupPageState();
@@ -11,30 +10,78 @@ class _GroupPageState extends State<GroupPage> {
 
   @override
   Widget build(BuildContext context) {
-    makeGrid(){
-      List<Widget> a=[];
-      a.add(Row(
-        children:[
-          Text("A"),
-          Text("b"),
-          Text("c"),
-        ]
-      ));
-
-      return a;
-    }
+    List<String> books = ['assets/images/literature.png', 'assets/images/literature.png', 'assets/images/literature.png', 'assets/images/literature.png', 'assets/images/literature.png'];
     return Scaffold(
       appBar: AppBar(
-        title: Text('Book Club'),
-        elevation: 0.0,
-        backgroundColor: colorScheme.color6,
-        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Flexible(
+              flex: 1,
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 16,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                  hintText: '도서명을 입력해주세요',
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          ...makeGrid(),
-        ],
-      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 50),
+        child: makeGrid(books),
+        ),
+      );
+  }
+}
+
+Column makeGrid(List<String> books){
+  List<Widget> grid_books = [];
+
+  for (int i = 0; i < books.length; i += 3){
+    List<Widget> row = [];
+    for (int j = i; j < i + 3; j++){
+      if (j >= books.length){
+        row.add(SizedBox(width: 100,));
+      }
+      else{
+        row.add(Image(
+          image: AssetImage(books[j]),
+          width: 100,
+          height: 120,
+        ));
+      }
+    }
+    grid_books.add(
+      Padding(
+        padding: const EdgeInsets.only(top: 50),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [...row],
+        ),
+      )
     );
   }
+
+  return Column(
+    children: grid_books,
+    );
 }
--- lib/system/apiSystem.dart
+++ lib/system/apiSystem.dart
@@ -1,28 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
-
-class User {
-  final int user_no;
-  final String user_id;
-  final String user_name;
-
-  User({
-    required this.user_no,
-    required this.user_id,
-    required this.user_name
-    });
-
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      user_no: json['user_no'],
-      user_id: json['user_id'],
-      user_name: json['user_name'],
-    );
-  }
-}
-
 
 Future<int> fetchUser(String user_id, String user_pw) async {
   var url = 'http://172.10.5.144:80/login';
