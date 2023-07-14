e3afb02aa551a531d2687483c51f774b600a9ebc
retro3014 <36194346+retro3014@users.noreply.github.com>
2023-07-14 21:14:38 +0900

    FEAT) init group info page

---
+++ lib/db/GroupEntity.dart
@@ -0,0 +1,8 @@
+class GroupEntity{
+  GroupEntity({required this.groupNo, required this.groupName, required this.groupBookNo, required this.groupStartDate, required this.groupEndDate});
+  int groupNo; 
+  String groupName;
+  int groupBookNo;
+  DateTime groupStartDate;
+  DateTime groupEndDate;
+}
